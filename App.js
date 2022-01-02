258dc5cf2817e4eeb95182722ca441f5858b8f74
SebTota <kidokyo1@gmail.com>
2022-01-02 10:04:48 -0500

    Create duolingo singin screen component

--- App.js
+++ App.js
@@ -9,9 +9,7 @@ import {
   extendTheme,
 } from 'native-base';
 
-this.state = {
-  dlUsername: '',
-};
+import DuoLingoSignIn from './screens/DuoLingoSignIn';
 
 const config = {
   useSystemColorMode: false,
@@ -20,38 +18,10 @@ const config = {
 
 const customTheme = extendTheme({config});
 
-function AppSignIn() {
-  return (
-    <Center flex={1} _dark={{bg: 'coolGray.800'}} _light={{bg: 'warmGray.50'}}>
-      <Heading textAlign="center" mb="15">
-        DuoLingo Flashcards
-      </Heading>
-      <Text fontSize="lg" display="flex" mb="5">
-        Enter your DuoLingo username
-      </Text>
-      <Input
-        placeholder="Username"
-        isFullWidth="true"
-        size="lg"
-        mb="5"
-        onChangeText={r => {
-          this.state.dlUsername = r;
-        }}
-      />
-      <Button
-        onPress={() => {
-          console.log(this.state.dlUsername);
-        }}>
-        Start Learning
-      </Button>
-    </Center>
-  );
-}
-
 const App = () => {
   return (
     <NativeBaseProvider theme={customTheme}>
-      <AppSignIn />
+      <DuoLingoSignIn />
     </NativeBaseProvider>
   );
 };
---
+++ screens/DuoLingoSignIn.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Heading, Button, Center, Text, Input} from 'native-base';
+
+const DuoLingoSignIn = () => {
+  this.state = {
+    dlUsername: '',
+  };
+
+  return (
+    <Center flex={1} _dark={{bg: 'coolGray.800'}} _light={{bg: 'warmGray.50'}}>
+      <Heading textAlign="center" mb="15">
+        DuoLingo Flashcards
+      </Heading>
+      <Text fontSize="lg" display="flex" mb="5">
+        Enter your DuoLingo username
+      </Text>
+      <Input
+        placeholder="Username"
+        isFullWidth="true"
+        size="lg"
+        mb="5"
+        onChangeText={r => {
+          this.state.dlUsername = r;
+        }}
+      />
+      <Button
+        onPress={() => {
+          console.log(this.state.dlUsername);
+        }}>
+        Start Learning
+      </Button>
+    </Center>
+  );
+};
+
+export default DuoLingoSignIn;
